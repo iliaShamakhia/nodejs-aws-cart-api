8ad0dc759cd383f46ecc5b8a0b8e2e1cbdcd654f
iliaShamakhia <shamakhiailia@gmail.com>
2025-04-12 17:15:44 +0400

    add dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 
-//import helmet from 'helmet';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
-//import { ConfigService } from '@nestjs/config';
-import serverlessExpress from '@codegenie/serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
+import { ConfigService } from '@nestjs/config';
+//import serverlessExpress from '@codegenie/serverless-express';
+//import { Callback, Context, Handler } from 'aws-lambda';
 
-let server: Handler;
+//let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  /* const configService = app.get(ConfigService);
+  const configService = app.get(ConfigService);
 
-  const port = configService.get('APP_PORT') || 4000; */
+  const port = configService.get('APP_PORT') || 4000;
+
   app.enableCors({
     origin: true,
   });
-  //app.use(helmet());
-  await app.init();
 
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+  app.use(helmet());
+  
+  //await app.init();
 
-  /* await app.listen(port, () => {
+  /* const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp }); */
+
+  await app.listen(port, () => {
     console.log('App is running on %s port', port);
-  }); */
+  });
 }
-//bootstrap();
+bootstrap();
 
-export const handler: Handler = async (
+/* export const handler: Handler = async (
   event: any,
   context: Context,
   callback: Callback,
 ) => {
   server = server ?? (await bootstrap());
   return server(event, context, callback);
-};
+}; */
